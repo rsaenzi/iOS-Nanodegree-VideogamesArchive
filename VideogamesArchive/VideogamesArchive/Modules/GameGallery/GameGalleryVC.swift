226adf4161e79ca76145597bbe7326c4d770821d
Rigoberto Saenz <beto456789@gmail.com>
2018-01-02 16:43:46 -0500

    GameGallery code base added

--- VideogamesArchive/VideogamesArchive/Modules/GameGallery/GameGalleryVC.swift
+++ VideogamesArchive/VideogamesArchive/Modules/GameGallery/GameGalleryVC.swift
@@ -7,7 +7,73 @@
 //
 
 import UIKit
+import KVNProgress
+import Kingfisher
 
 class GameGalleryVC: UIViewController {
     
+    @IBOutlet weak var collection: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        KVNProgress.show()
+        RequestGetGameInfo.request(gameId: 1942) { response in
+            
+            KVNProgress.dismiss()
+            switch response {
+                
+            case .success(let output):
+                print("Success")
+                break
+                
+            default:
+                print("Error")
+                break
+//                self?.showSimpleAlert(title: "Error", message: "Error when trying to fetch all the Game Engines")
+            }
+        }
+    }
+    
+}
+
+extension GameGalleryVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+
+extension GameGalleryVC: UICollectionViewDelegate {
+    
+}
+
+extension GameGalleryVC: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return collectionView.bounds.size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets.zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize.zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize.zero
+    }
 }
